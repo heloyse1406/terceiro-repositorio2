8d79aaf507eefa78e5bd36e79522ea952b9f364d
heloyse1406 <heloyse.camile@escola.pr.gov.br>
2025-06-16 08:59:44 -0300

    aula 4

--- main.js
+++ main.js
@@ -4,5 +4,19 @@
 
  const botoes = document.querySelectorAll('.parametro-senha_botao');
 
- console.log(botoes);
- 
+ botoes[0].onclick = diminuiTamanho;
+ botoes[1].onclick = aumentaTamanho;
+
+  function diminuiTamanho() {
+       if (tamanhoSenha > 1) {
+           tamanhoSenha = tamanhoSenha -1;
+     }
+     numeroSenha.textContent = tamanhoSenha;
+  }
+
+  function aumentaTamanho() {
+    if(tamanhoSenha < 20) {
+        tamanhoSenha = tamanhoSenha +1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+  }
