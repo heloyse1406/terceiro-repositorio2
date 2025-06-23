235c515ec0378a1cb59a9893be53437d40664692
heloyse1406 <heloyse.camile@escola.pr.gov.br>
2025-06-23 09:04:31 -0300

    aula 5

--- main.js
+++ main.js
@@ -9,14 +9,30 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
-        tamanhoSenha = tamanhoSenha - 1;
+        tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 function aumentaTamanho() {
     if (tamanhoSenha < 20){
-        tamanhoSenha = tamanhoSenha + 1;
+        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const letraMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    let senha = '' ;
+    for(let i=0; i < tamanhoSenha; i++ ) {
+        let numeroAleatorio = Math.random()*letraMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letraMaiusculas[numeroAleatorio];
+    }
+   campoSenha.value = senha;
 }
