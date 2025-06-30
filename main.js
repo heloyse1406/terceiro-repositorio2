190b608c9987835cf72c06ec21f865ca30310e93
heloyse1406 <heloyse.camile@escola.pr.gov.br>
2025-06-30 08:13:06 -0300

    aula 6

--- main.js
+++ main.js
@@ -24,15 +24,35 @@ function aumentaTamanho() {
 }
 
 const campoSenha = document.querySelector('#campo-senha');
-const letraMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const checkbox = document.querySelectorAll('.checkbox');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
 geraSenha();
 
 function geraSenha(){
+    let alfabeto ='';
+    if (checkbox[0].cheked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].cheked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].cheked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].cheked){
+        alfabeto = alfabeto + simbolos;
+    }
+
     let senha = '' ;
     for(let i=0; i < tamanhoSenha; i++ ) {
-        let numeroAleatorio = Math.random()*letraMaiusculas.length;
+        let numeroAleatorio = Math.random()*alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letraMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
     }
    campoSenha.value = senha;
 }
